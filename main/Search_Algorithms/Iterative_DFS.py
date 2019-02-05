fbc99c63be9b37aed1fe3fe74d5353fcee71ac53
Giovanni Ruiz Ortiz <termigiova@me.com>
2019-02-04 21:47:33 -0600

    Creation - Created the Iterative DFS module, this works the same as DFS but it works by using a stack, which in the end is going to alter the order of the elements that will be stored in the 'result'

---
+++ main/Search_Algorithms/Iterative_DFS.py
@@ -0,0 +1,47 @@
+from queue import Queue
+
+from main.Search_Algorithms.DefaultGraph import DefaultGraph
+
+
+class Iterative_DFS:
+
+    result = []
+    stack = Queue()
+
+    def __init__(self):
+        defaultGraph = DefaultGraph()
+        defaultGraph.graph.printGraph()
+        self.graph = defaultGraph.getGraph()
+        self.visitedVertex = self.initializeVisitedVertex()
+
+    def initializeVisitedVertex(self):
+        visitedVertex = {}
+        for key in self.graph.keys():
+            visitedVertex[key] = False
+        return visitedVertex
+
+    def DFS(self, startingVertex):
+        self.stack.put(startingVertex)
+        self.findDFS()
+
+
+    def findDFS(self):
+        if not self.stack.empty():
+            currentVertex = self.stack.get()
+            if not self.visitedVertex[currentVertex]:
+                self.visitedVertex[currentVertex] = True
+                self.result.append(currentVertex)
+
+            for vertex in self.graph[currentVertex]:
+                if not self.visitedVertex[vertex.name]:
+                    self.stack.put(vertex.name)
+
+            self.findDFS()
+
+    def printResult(self):
+        for item in self.result:
+            print(item, end=' -> ')
+
+graph = Iterative_DFS()
+graph.DFS('Acapulco')
+graph.printResult()
